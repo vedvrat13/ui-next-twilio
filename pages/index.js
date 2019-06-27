3cf108d70c578f5a677283eb4e5e499821975ee2
VEDVRAT SHARMA <vedvrat@indebted.io>
2019-06-27 22:12:52 +1000

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Container from '@material-ui/core/Container'
+import AppBar from '../src/AppBar'
+
+export default function Index() {
+  return (
+    <Container maxWidth="lg">
+      <AppBar />
+    </Container>
+  )
+}
---
+++ src/appConfig.js
@@ -0,0 +1,23 @@
+const appConfig = {
+  accountSid: 'AC747dccdc366a097a50dc38440e94392e',
+  flexFlowSid: 'FOdaab057dd8af92ab7e1c4dec387e4860',
+  context: {
+    friendlyName: 'Indebted'
+  },
+  colorTheme: {
+    overrides: {
+      EntryPoint: {
+        Container: {
+          left: '10px',
+          bottom: '60px',
+          'max-width': '155px'
+        }
+      },
+      MainContainer: {
+        left: '5%'
+      }
+    }
+  }
+}
+
+export default appConfig
